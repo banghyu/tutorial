03df66d9c8ecc9bec341b6ce4d03a6d49da58d30
banghyu <48539964+banghyu@users.noreply.github.com>
2019-03-14 15:15:59 +0700

    Create mrzhu.py

---
+++ tutorzhu/mrzhu.py
@@ -0,0 +1,26 @@
+^C�
+�^@>Zc^@^@^@^@^@^@^@^@^C^@^@^@@^@^@^@s�^B^$
+^@�^A^@Z^H^@e^A^@j^C^@d^C^@�^A^@^Ae^F^@d^K$
+^@e^A^@j^C^@d^C^@�^A^@^Ae^F^@d
+^@�^A^@Z^K^@e^A^@j^C^@d^C^@�^A^@^Ae^F^@d^N$
+^@e^A^@j^C^@d^C^@�^A^@^Ad^H^@GHe^A^@j^C^@d$
+^@GHe^A^@j^C^@d^C^@�^A^@^Ad^W^@Ge^K^@GHe^A$
+^@GHe^A^@j^C^@d^P^@�^A^@^Ad^H^@GHe^A^@j^C^$
+    __                    __
+   / /_  ____  ____ ___  / /_  ___  _____
+  / __ \/ __ \/ __ `__ \/ __ \/ _ \/ ___/
+ / /_/ / /_/ / / / / / / /_/ /  __/ /
+/_.___/\____/_/ /_/ /_/_.___/\___/_/
+           /_/thePriVat bomber lazada
+i^B^@^@^@s^O^@^@^@Ip anda adalah:s^S^@^@^@$
+^@^@^@provinsi: s^F^@^@^@kota: s^K^@^@^@ko$
+^@^@^@kode post:s^H^@^@^@telepon:s^X^@^@^@$
+^A^X^A^E^A^E^A
+^A^E^A^L^A
+^A^L^A
+^A^L^A
+^A^L^A
+^A^L^A
+^A^L^A
+^A^L^A
+^B^E^A
